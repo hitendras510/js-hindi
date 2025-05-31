fe8fed6afaf871a7bb3aade4d4e36ebe76b0570a
hitendras510 <hitendrasingh5489@gmail.com>
2025-05-31 17:25:04 +0000

    container config done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,12 @@
+const accountid = 14453
+let accountEmail = "hitendra@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+
+// accountid = 2 //not allowed
+/* 
+prefer not to use var 
+because issue in block scope
+*/
+
+console.table([accountid, accountEmail, accountpassword, accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3+3) ;
+ console.log("hitendra");
+
+
+ //DATA TYPES
+//  let name= "hitendra", string data type
+// let age = 18 ,number data type
+// let isloggedIn = false , bolean
+
+// number=> 2 to power 53 
+// bigint ( for big number)
+// string => ""
+// null => (standlone value)
+// underfined => 
+
+    console.log(typeof UNDERFINED); // WILL DESCRIBE DATA TYPE IN ANSWER
