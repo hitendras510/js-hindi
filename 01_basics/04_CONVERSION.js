784e398202e454f67919caf459e023c8f2ef97f0
hitendras510 <hitendrasingh5489@gmail.com>
2025-06-01 12:54:03 +0000

    container config done

---
+++ 01_basics/04_CONVERSION.js
@@ -0,0 +1,41 @@
+//   console.log(+""); //0
+//   let num1, num2, num3
+
+//   num1 = num2 = num3 = 2+2
+//   let gamecounter = 100
+//   gamecounter++;
+//   console.log(gamecounter;) 
+// 101
+
+
+// comparison of data types
+
+//  console.log("2">1); true
+//  console.log("02">1); true
+
+
+
+console.log("2" === 2);
+
+//strictly avoid these
+
+// console.log(null>0); false
+// console.log(null==0); false 
+// console.log(null>=0); true 
+// console.log(underfined == 0);
+// console.log(underfined > 0);
+// console.log(underfined < 0);
+
+// DATA TYPES SUMMARY
+// primitive type
+// 7 types : string , number , boolearn , null , undfined , symbol , bigint
+
+// reference type 
+// array , object ,funtions
+
+///////////////////////////////////////////
+
+// stack ( primitive) , Heap ( non primitive)
+// example
+let myyoutubename = "hitendrasingh"
+let anothername = myyoutubename
---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+// In JavaScript, you can choose single quotes ('),
+//  double quotes ("), or backticks (`) to wrap your
+//  strings in. All of the following will work:
+// Strings declared using backticks are a special
+//  kind of string called a template literal.
+
+
+const name = "hitendra"
+const repocount = 50
+
+// console.log(name + repocount + "value "); it is outdated
+
+// use this below one
+console.log(` Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String(`hitendra`)
+console.log(gamename[0]);
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt());
+console.log(gamename.indexOf('d'));
+
+// const newString = gamename.slice(-8,4)
+// console.log(anotherstring); this is another way of describing the sentences
+// you can even enter negative value it will assign in reverse order
+
+// another method trim or replace
+
+const newstringone = " hitendra   "
+console.log
