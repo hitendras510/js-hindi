a6d8147cfae662d77b64141a12035059b96ccaa4
Hitendra  Singh <hitendrasingh5489@gmail.com>
2025-06-04 15:42:49 +0000

    config container done

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,36 @@
+//  const score = 6969
+//  console.log(score);
+
+//  const balance = new Number(100)
+//  console.log(balance);
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(1));
+//  const othernumber = 23.8966
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+///////////////// maths//////////////////
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+//  for positive
+ console.log(Math.round(4.367));
+//   round of
+console.log(Math.ceil(4.12234));
+// it will result 5  
+console.log(Math.floor(4.12234));
+// vice versa of ceil
+
+console.log(Math.random());
+// always result in btw 0-1
+
+console.log((Math.random()*10) +1);
+// random answer
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()* ( max - min + 1)) + min)
+ 
---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,12 @@
+let mydate = new Date()
+console.log(mydate.toString());
+
+//console.log(typeof mydate); date is an object
+
+// to declare date
+// let mycreateddate = new date (2025, 2, 23)
+//console.log(mycreateddate.tolocalestring());
+
+let newdate = new date()
+console.log(newdate);
+console.log(newdate.getmonth());
