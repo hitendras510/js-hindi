7d2888a51bac951de36ff1f5030651be395fe038
Hitendra  Singh <hitendrasingh5489@gmail.com>
2025-06-05 18:26:31 +0000

    container config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,30 @@
+// array , you can store multiple items in a single variable 
+// numbers string bolean etc 
+
+const myArr = [ 0, 1, 2,3,4,5]
+const myheors = [ " virat"];
+
+// declaring array
+
+console.log(myArr[0]);
+
+// array methods , DON'T USE SMALL A IN myArr
+
+myArr.push(9)
+myArr.pop() // use to remove last stored value
+console.log(myArr);
+
+myArr.unshift(9) // to add value in 1st digit of stored value
+
+console.log(myArr.includes(9)); // weather the value is stored or not
+ 
+// to convert array into string
+const newArr = myArr.join() 
+ 
+// slice  shows the first required stored values
+//  splice shows the last required stored values
+
+//////////////////////////////////////////////////////////////////////
+
+
+
