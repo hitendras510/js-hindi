e19999a282244be1aafeaebcdbd226bf1846c7c4
Hitendra  Singh <hitendrasingh5489@gmail.com>
2025-06-06 18:26:55 +0000

    container config done

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,15 +1,37 @@
-// singleton ,,object types
+// // singleton ,,object types
 
-// to access required thing from set of information
+// // to access required thing from set of information
 
-const JsUser = { 
-    name: "hitendra",
-    age: 18,
-    location: "jabalpur",
-    email: "hitiendrasingh5489@gmail.com",
-    isLoggendIn: false,
-}
+// const JsUser = { 
+//     name: "hitendra",
+//     age: 18,
+//     location: "jabalpur",
+//     email: "hitiendrasingh5489@gmail.com",
+//     isLoggendIn: false,
+// }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
 
+// JsUser.greeting = function(){
+
+// }
+// refer mdn doocs for notes
+
+////////////////////////////////////////////
+
+// const regularUser = {
+
+//     email: "hitendra@9425",
+//     fullname: {
+//         userfullname: {
+//             firstname: "hitendra",
+//     lastname: "singh"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.userfullname)
+
+// to check is property (eg. isLoggedIn) available in the storage
+
+// console.log(tinerUser.hasOwnProperty('isLoggendIn'));
