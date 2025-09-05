24e95edb199ebd9fbf02b4aa39d4f834bb65558c
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-05 10:51:13 +0530

    new

--- 01_basics/loop.js
+++ 01_basics/loop.js
@@ -18,12 +18,12 @@ do{
     takeNumber = numbers[l] * 2;
     multipliedNumbers.push(takeNumber);
   }
-//   console.log(multipliedNumbers);
-// //////////////////////////////////////////
+  console.log(multipliedNumbers);
+//////////////////////////////////////////
 
 
 let cityList=[]
-let cities = ["paris","new york" ,"tokyo"];
+// let cities = ["paris","new york" ,"tokyo"];
 
 
 for (let c = 0; c < cities.length; c++) {
@@ -31,3 +31,70 @@ for (let c = 0; c < cities.length; c++) {
     cityList.push(myCity);
 }
 console.log(cityList);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+4. write a for loop that loops through ( green tea and black tea and chai and oolong tea) and stops the loop when it finds chai , store all teas before chai in a new array named selectedTeas
+ 
+let teas = ["green tea", "black tea", "chai", "oolong tea" ];
+let selectedTeas = []
+ 
+for( let i = 0; i < teas.length; i++){
+  if(teas[i]== 'chai'){
+    break;
+  }
+  selectedTeas.push(teas[i])
+}
+console.log("selectedTeas")
+
+
+
+/////////////////////////////////////////
+
+ let cities = ["london","New york","Paris","berlin"];
+ let visitedCities=[];
+
+ for (let i = 0; i < cities.length; i++) {
+  if(cities[i] === " paris"|| cities[i]=== "Paris"){
+    continue; 
+  }
+ visitedCities.push(cities[i])
+  
+ }
+ console.log(visitedCities);
+
+//////////////////////////////////////////////////////////////
+
+let numbers=[1,2,3,4,5];
+let smallNumbers=[];
+
+for( const num of numbers){
+  if (num === 4){
+    break;
+  }
+  smallNumbers.push(num);
+}
+console.log(smallNumbers);
+///////////////////////////////////////////////////
+
+let worldCites = {
+  "sydney": 5000000,
+  "Tokyo": 9000000,
+  "Berlin": 3500000,
+  "Paris": 2200000,
+}
+let largeCites = {};
+
+for (const city in worldCites) {
+  if (worldCites[city] < 3000000) {
+    continue;
+  }
+  largeCites[city] = worldCites[city];
+}
+console.log(largeCites);  
+
+ /////////////////////////////////////////
+
+ let teaCollection = ["earl grey", "green tea","chai", "oolong tea"];
+ let availableTeas = []
+
