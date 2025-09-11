fd698ca529ae91897d342ebdd8a849b49f820630
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-11 23:38:08 +0530

    new

--- project_1/script.js
+++ project_1/script.js
@@ -18,18 +18,38 @@ addTaskButton.addEventListener('click', () => {
     };
     tasks.push(newTask)
     saveTasks();
+    renderTask(newTask);
     todoInput.value=""; // clear input
     console.log(tasks);
 });
 
 function renderTask(task){
-    console.log(task);
-}
+   const li = document.createElement("li");
+   li.setAttribute("data-id", task.id);
+   if(task.completed) li.classList.add()
+    li.innerHTML = `
+<span>${task.text}</span>
+<button>delete</button>
+`;
+li.addEventListener("click", (e) => {
+     if(e.target.tagName === "BUTTON" ) return;
+     task.completed = !task.completed
+     li.classList.toggle("completed")
+     saveTasks();
+});
 
+li.querySelector('button').addEventListener('click', (e) => {
+    e.stopPropagation()
+    tasks = tasks.filter(t => t.id !== task.id);
+    li.remove();
+    saveTasks();
+});
+ 
+todoList.appendChild(li);
+}
 
 
 function saveTasks(){
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
 });
